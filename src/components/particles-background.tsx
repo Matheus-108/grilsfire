17eec4b86ef55f15f06cccbc4b7818d49a985f03
Matheus Burgos <burgosmatheusc@gmail.com>
2025-07-02 15:29:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/particles-background.tsx
+++ src/components/particles-background.tsx
@@ -4,7 +4,7 @@
 import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import type { Container, ISourceOptions } from "@tsparticles/engine";
-import { loadSlim } from "tsparticles-slim";
+import { loadSlim } from "@tsparticles/slim";
 
 export function ParticlesBackground() {
   const [init, setInit] = useState(false);
