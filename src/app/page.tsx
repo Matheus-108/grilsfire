3da19fdfdb10fda4eddf164a2c25433c1aa33623
Matheus Burgos <burgosmatheusc@gmail.com>
2025-08-28 19:41:20 +0000

    não veio imagem  nenhuma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -258,7 +258,7 @@ const ReviewCard = ({ review }: { review: Review }) => {
 const VideoFeed = () => (
   <div className="relative w-full max-w-sm mx-auto aspect-[9/16] rounded-2xl overflow-hidden shadow-2xl shadow-red-500/30 border-2 border-gray-700 my-8">
     <Image 
-      src="https://i.imgur.com/DDAzojm.png"
+      src="https://i.imgur.com/uG2GZJ6.png"
       alt="Video feed"
       layout="fill"
       objectFit="cover"
